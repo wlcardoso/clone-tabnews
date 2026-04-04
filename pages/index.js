38882a4e57d35c0b5cd1b6265d677e49ef63d66a
Washington Cardoso <washingtonlsid@gmail.com>
2026-04-03 23:59:27 -0300

    restaurando projeto após desafio

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-    return ( 
-      <div>
-        <h1>🌷 Fabi, você é maravilhosa! Eu te amo 😻!! Bora no Outback?? </h1>
-        <img src="/apenas-um-show.gif" alt="GIF animado" />
-      </div>
-    );
+    return <h1>Lolo e Agnes, minhas princesas! Amo vocês !!</h1>
 }
 
 export default Home;
