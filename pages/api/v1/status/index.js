feb9e54f5169570998525a4a7773eb67928d7edd
Washington Cardoso <washingtonlsid@gmail.com>
2026-04-21 13:45:31 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,22 +3,27 @@ import database from "infra/database.js";
 async function status (request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersion = await database.query('SELECT version();')
-  const version = databaseVersion.rows[0].version.split(" ")[1]
+  const databaseVersionResult = await database.query('SHOW server_version;')
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version
 
-  const databaseMaxConn = await database.query('SHOW max_connections;')
-  const maxConn = parseInt(databaseMaxConn.rows[0].max_connections)
+  const databaseMaxConnectionResult = await database.query('SHOW max_connections;')
+  const databaseMaxConnectionValue = parseInt(databaseMaxConnectionResult.rows[0].max_connections)
 
-  const databaseUsedConn = await database.query("SELECT count(*) FROM pg_stat_activity WHERE state = 'active';")
-  const usedConn = parseInt(databaseUsedConn.rows[0].count)
+  const databaseName = process.env.POSTGRES_DB
+  const databaseOpenedConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+});
+
+  const databaseOpenedConnectionValue = databaseOpenedConnectionResult.rows[0].count
   
   response.status(200).json({
     updated_at: updatedAt, 
     dependencies: {
       database: {
-        version: version,
-        max_connections: maxConn,
-        opened_connections: usedConn,
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionValue,
+        opened_connections: databaseOpenedConnectionValue,
       }
     }
   })
