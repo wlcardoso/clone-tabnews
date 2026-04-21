8ab37c8412d61aa93f5ad443fab0955fbb106919
Washington Cardoso <washingtonlsid@gmail.com>
2026-04-21 13:44:33 -0300

    make `database.js` more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,4 @@
-import { Client } from 'pg';
+import { Client, Result } from 'pg';
 
 async function query(queryObject) {
   const client = new Client({
@@ -7,11 +7,19 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  })
+  
+  await client.connect()
+
+  try {  
+    const result = await client.query(queryObject)   
+    return result
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end()
+  }
+ 
 }
 
 export default {
