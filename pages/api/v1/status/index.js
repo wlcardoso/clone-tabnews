1251f84941e370e3587f7632185c540fd67e9ad9
Washington Cardoso <washingtonlsid@gmail.com>
2026-04-20 23:11:44 -0300

    improving `/status` with database informations

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 async function status (request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.rows);
-  response.status(200).json({ version: "1" })
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await database.query('SELECT version();')
+  const version = databaseVersion.rows[0].version.split(" ")[1]
+
+  const databaseMaxConn = await database.query('SHOW max_connections;')
+  const maxConn = parseInt(databaseMaxConn.rows[0].max_connections)
+
+  const databaseUsedConn = await database.query("SELECT count(*) FROM pg_stat_activity WHERE state = 'active';")
+  const usedConn = parseInt(databaseUsedConn.rows[0].count)
+  
+  response.status(200).json({
+    updated_at: updatedAt, 
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: maxConn,
+        opened_connections: usedConn,
+      }
+    }
+  })
 }
 
-export default status
+export default status;
