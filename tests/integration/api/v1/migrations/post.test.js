ff279da05ceebf4a222b463b9c3ddbf958ec7653
Washington Cardoso <washingtonlsid@gmail.com>
2026-04-29 00:31:52 -0300

    fixed code formatting

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,30 +1,28 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
-beforeAll(cleanDatabase)
+beforeAll(cleanDatabase);
 async function cleanDatabase() {
-  await database.query("DROP schema public cascade; CREATE schema public;")
+  await database.query("DROP schema public cascade; CREATE schema public;");
 }
 
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: 'POST',
-  })
+    method: "POST",
+  });
   expect(response1.status).toBe(201);
 
   const response1Body = await response1.json();
 
-  expect(Array.isArray(response1Body)).toBe(true)
-  expect(response1Body.length).toBeGreaterThan(0)
-  
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
+
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: 'POST',
-  })
+    method: "POST",
+  });
   expect(response2.status).toBe(200);
 
   const response2Body = await response2.json();
 
-  expect(Array.isArray(response2Body)).toBe(true)
-  expect(response2Body.length).toBe(0)
-  
-  
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
