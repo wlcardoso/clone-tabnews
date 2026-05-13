48ffbd02769cac1e163dc4f1da7c9d6e568a23d1
WASHINGTON <wlcardoso@users.noreply.github.com>
2026-05-13 20:11:02 +0000

    fix `eslint` linting

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,4 @@
-import { Client, Result } from "pg";
+import { Client } from "pg";
 
 async function query(queryObject) {
   let client;
@@ -27,11 +27,12 @@ async function getNewClient() {
   await client.connect();
   return client;
 }
-
-export default {
+const database = {
   query,
   getNewClient,
 };
+
+export default database;
 
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
